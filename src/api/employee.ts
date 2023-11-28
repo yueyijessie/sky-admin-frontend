b709d28cd8d13e3cf70b8012e20c94ec7638c3c0
yueyijessie <welhomejessie@gmail.com>
2023-11-28 16:05:36 +0800

    添加查询员工接口

--- src/api/employee.ts
+++ src/api/employee.ts
@@ -12,10 +12,18 @@ export const login = (data: any) =>
     data: data
   })
 
-  // 退出
- export const userLogout = (params: any) =>
- request({
-   'url': `/employee/logout`,
-   'method': 'post',
-   params
- })
+// 退出
+export const userLogout = (params: any) =>
+request({
+  'url': `/employee/logout`,
+  'method': 'post',
+  params
+})
+
+// 分页查询
+export const getEmployeeList = (params: any) =>
+request({
+  'url': `/employee/page`,
+  'method': 'get',
+ params
+})
