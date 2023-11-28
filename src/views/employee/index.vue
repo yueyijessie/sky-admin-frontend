3c9ba296b39f41791b49453dd7c27ff420724dd1
yueyijessie <welhomejessie@gmail.com>
2023-11-28 16:06:07 +0800

    员工管理页面

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -1,15 +1,106 @@
 <template>
   <div class="dashboard-container">
     <div class="container">
-
+      <div class="tableBar">
+        <label style="margin-right: 5px;">员工姓名：</label>
+        <el-input v-model="name" placeholder="请输入员工姓名" style="width: 15%;" />
+        <el-button type="primary" style="margin-left: 25px;" @click="pageQuery">查询</el-button>
+        <el-button type="primary" style="float:right">+ 添加员工</el-button>
+      </div>
+      <el-table
+        :data="records"
+        stripe
+        style="width: 100%">
+        <el-table-column
+          prop="name"
+          label="员工姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="username"
+          label="账号"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="phone"
+          label="手机号">
+        </el-table-column>
+        <el-table-column
+          prop="status"
+          label="账号状态">
+          <template slot-scope="scope">
+            {{ scope.row.status === 0 ? '禁用' : '启用'}}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="updateTime"
+          label="最后操作时间">
+        </el-table-column>
+        <el-table-column
+          label="操作">
+          <template slot-scope="scope">
+            <el-button type="text">修改</el-button>
+            <el-button type="text">{{ scope.row.status === 1 ? '禁用' : '启用'}}</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+       class="pageList"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="page"
+        :page-sizes="[5, 10]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
     </div>
-
   </div>
 </template>
 <script lang="ts">
-
+import {getEmployeeList} from '@/api/employee'
 export default  {
-  
+  data() {
+    return {
+      records: [], // 当前要展示的数据集合
+      total: 0,
+      page: 1,
+      pageSize: 5,
+      name: ''
+    }
+  },
+  created(){
+    this.pageQuery()
+  },
+  methods:{
+    // 分页查询
+    pageQuery(){
+      // 准备请求参数
+      const params = {
+        name: this.name,
+        page: this.page,
+        pageSize: this.pageSize
+      }
+      // 发送ajax请求，访问后端服务，获取分页数据
+      getEmployeeList(params).then(res => {
+        if (res.data.code === 1){
+          this.total = res.data.data.total
+          this.records = res.data.data.records
+        }
+      }).catch(err => {
+        this.$message.error('请求出错了：' + err.message)
+      })
+    },
+    handleSizeChange(val) {
+      this.pageSize = val
+      this.page = 1
+      this.pageQuery()
+    },
+    handleCurrentChange(val) {
+      this.page = val
+      this.pageQuery()
+    }
+  }
 }
 </script>
 
